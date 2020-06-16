1ff8927dfa926d7239332d20297b4ea36b50c185
Christian <christian.w@hotmail.nl>
2020-06-16 23:01:26 +0200

    added comments

--- main.c
+++ main.c
@@ -4,13 +4,13 @@
 
 int main()
 {
-    bool plus = false;
-    double pi = 1;
-    int piIncrement = 3;
-    int terms;
+    bool plus = false;                      //determines add (+) or substract (-)
+    double pi = 1;                          //to use Leibniz's formula, start formula with 1
+    int piIncrement = 3;                    //3-5-7-9 (Leibniz's formula)
+    int terms;                              //times to calculate Pi, the more terms, the more accurate Pi is calculated
 
     printf("Enter amount of terms to calculate: ");
-    scanf("%d", &terms);
+    scanf("%d", &terms);                    //using %d to get integer, other values like strings are still allowed and will still work (no clue why)
 
     for (int i = 1; i <= terms; i += 1)
     {
@@ -25,9 +25,9 @@ int main()
                 printf("+ 1 / %d = %f \n", piIncrement, pi);
                 break;
         }
-        plus = !plus;
-        piIncrement += 2;
+        plus = !plus;                       //set bool 'plus' to value that is not the existing value
+        piIncrement += 2;                   //3-5-7-9-etc
     }
 
-    printf("%f * 4 =\n%f\n", pi, pi * 4);
+    printf("%f * 4 =\n%f\n", pi, pi * 4);   //using %f to display double
 }
